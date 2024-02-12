bf283c038ae32526fc7b5521bb73d679407fbd09
jonsantosdev <js.jsantos.jr@gmail.com>
2024-02-12 12:32:03 -0800

    readme file added and set character limit to posts

--- public/js/post.js
+++ public/js/post.js
@@ -5,6 +5,11 @@ const newPostHandler = async (event) => {
     const artist_name = document.querySelector('#artist-name').value.trim();
     const genre = document.querySelector('#genre').value.trim();
 
+    if (song_title.length > 100 || artist_name.length > 100 || genre.length > 100) {
+      alert('Song title, artist name, and genre must be no more than 100 characters.');
+      return;
+    }
+        
     if (song_title && artist_name && genre) {
       const response = await fetch(`/api/music`, {
         method: 'POST',
